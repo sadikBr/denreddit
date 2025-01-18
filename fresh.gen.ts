62186ac802c10d7014c67f65539d44bb2a6fbf29
sadikBr <sadbrah@gmail.com>
2025-01-18 23:56:29 +0100

    initialize the creation of the user page

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,6 +6,7 @@ import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $index from "./routes/index.tsx";
 import * as $subreddit_subreddit_ from "./routes/subreddit/[subreddit].tsx";
+import * as $user_user_ from "./routes/user/[user].tsx";
 import * as $posts from "./islands/posts.tsx";
 import * as $search_results from "./islands/search-results.tsx";
 import type { Manifest } from "$fresh/server.ts";
@@ -16,6 +17,7 @@ const manifest = {
     "./routes/_app.tsx": $_app,
     "./routes/index.tsx": $index,
     "./routes/subreddit/[subreddit].tsx": $subreddit_subreddit_,
+    "./routes/user/[user].tsx": $user_user_,
   },
   islands: {
     "./islands/posts.tsx": $posts,
